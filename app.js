299dff2142494ca93bbccbd71eac0b57919b2c5e
Khailas R <khailas303@gmail.com>
2021-10-20 13:25:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let inputBt = document.getElementById('input-btn');
+
+
+inputBt.addEventListener('click', () => {
+    console.log('btn clicked');
+});     // this is used instead of onclick in html
+
