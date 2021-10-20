5d62c0ba696351e049eee2a56dca7eafe33ba8fb
Khailas R <khailas303@gmail.com>
2021-10-20 16:13:03 +0530

    updated

--- app.js
+++ app.js
@@ -2,25 +2,34 @@ const inputBt = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const unorderedEl = document.getElementById('ul-el');
 
-let myLeads = ['www.myextension.com', 'www.ext.com', 'www.whatever.com'];
+let myLeads = [];
 
 
 // this is used instead of onclick in html
 inputBt.addEventListener('click', () => {
-    console.log('btn clicked');
-});
-
-
-inputEl.addEventListener('text', () => {
     myLeads.push(inputEl.value);
+    inputEl.value = '';
+    renderLeads();
 });
 
-for (let i=0; i<myLeads.length; i++) {
-    // rendering .textContent with .innerHtml 
-    // unorderedEl.innerHTML += "<li>" + myLeads[i] + "</li>";
 
-    // this works exactly like the commented one above
-    const li = document.createElement('li');
-    li.textContent = myLeads[i];
-    unorderedEl.append(li)
+// for (let i=0; i<myLeads.length; i++) {
+//     // rendering .textContent with .innerHtml 
+//     // unorderedEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+
+//     // this works exactly like the commented one above
+//     const li = document.createElement('li');
+//     li.textContent = myLeads[i];
+//     unorderedEl.append(li)
+// }
+
+
+// simplified version of the prior one
+function renderLeads() {
+    let listItems = '';
+    for (let i=0; i<myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>";
+    }
+    
+    unorderedEl.innerHTML = listItems
 }
